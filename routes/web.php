ae33de75b85108fb1413aac575700b6f6672d57b
marinasvirel <marusenka_83@mail.ru>
2025-04-03 02:44:38 +0300

    группировка маршрутов

--- routes/web.php
+++ routes/web.php
@@ -17,40 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// №1
-Route::get('/user/all', function () {
-    return 'all';
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        return 'all';
+    });
+    Route::get('/user/{id}', function ($id) {
+        return $id;
+    });
 });
-Route::get('/user/{id}', function ($id) {
-    return 'id';
-});
-
-// №2
-Route::get('/user/all', function () {
-    return 'all';
-});
-Route::get('/user/{id?}', function ($id = null) {
-    return 'id';
-});
-Route::get('/user/', function () {
-    return 'user';
-});
-
-// №3
-Route::get('/user/all', function () {
-    return 'all';
-});
-Route::get('/user/all/desc', function () {
-    return 'all desc';
-});
-Route::get('/user/{name}/{id?}', function ($name, $id) {
-    return 'name id';
-});
-
-// №4
-Route::get('/user/{id}', function ($id) {
-    return 'id';
-})->where('slug', '[a-z0-9_-]+');
-Route::get('/user/{id}', function ($id) {
-    return 'id';
-})->where('id', '[0-9]+');
