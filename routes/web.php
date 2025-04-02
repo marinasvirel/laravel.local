acd92a7c7d6d071fa6a691abc9e6d2634e0d6948
marinasvirel <marusenka_83@mail.ru>
2025-04-03 02:37:12 +0300

    разрешение конфликтов

--- routes/web.php
+++ routes/web.php
@@ -17,61 +17,40 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/test', function () {
-    return 'Сообщение: test';
+// №1
+Route::get('/user/all', function () {
+    return 'all';
+});
+Route::get('/user/{id}', function ($id) {
+    return 'id';
 });
 
-Route::get('/dir/test', function () {
-    return 'Сообщение: dir/test';
+// №2
+Route::get('/user/all', function () {
+    return 'all';
+});
+Route::get('/user/{id?}', function ($id = null) {
+    return 'id';
+});
+Route::get('/user/', function () {
+    return 'user';
 });
 
-// Route::get('/user/{name}', function ($name) {
-//     return "string $name";
-// });
-
-// Route::get('/user/{surname}/{name}', function ($surname, $name) {
-//     return "Имя: $name Фамилия: $surname";
-// });
-
-// Route::get('/city/{city?}', function ($city = 'minsk') {
-//     return "Город: $city";
-// });
-
-// Route::get('/user/{id}', function ($id) {
-//     return 'юзер ' . $id;
-// })->where('id', '[0-9]+');
-
-
-// Route::get('/user/{id}/{name}', function ($id, $name) {
-//     return 'юзер ' . $id . ' ' . $name;
-// })->where('id', '[0-9]+')->where('name', '[a-z]{3,}');
-
-// Route::get('/user/{order}', function ($order) {
-//     return "Юзер: $order";
-// })->where('order', 'name|surname|age');
-
-// Route::get('/posts/{date}', function ($date) {
-//     return "Пост: $date";
-// })->where('date', '^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$');
-
-// Route::get('/{year}/{month}/{day}', function ($year, $month, $day) {
-//     return "$year-$month-$day";
-// })->where('year', '\d{4}')->where('month', '(0[1-9]|1[0-2])')->where('day', '(0[1-9]|[12][0-9]|3[01])');
-
-
-// Route::get('user/{id}', function($id){
-//     return "User: $id";
-// })->whereNumber('id');
-
-// Route::get('city/{name}', function($name){
-//     return "Город: $name";
-// })->whereAlpha('name');
-
-Route::get('/post/{id}', function ($id) {
-    return '!!!'; // только если число
+// №3
+Route::get('/user/all', function () {
+    return 'all';
+});
+Route::get('/user/all/desc', function () {
+    return 'all desc';
+});
+Route::get('/user/{name}/{id?}', function ($name, $id) {
+    return 'name id';
 });
 
-Route::get('/posts/{slug}', function ($slug) {
-    return '!!!'; // буквы и цифры, а также дефис и подчеркивание
-});
+// №4
+Route::get('/user/{id}', function ($id) {
+    return 'id';
+})->where('slug', '[a-z0-9_-]+');
+Route::get('/user/{id}', function ($id) {
+    return 'id';
+})->where('id', '[0-9]+');
