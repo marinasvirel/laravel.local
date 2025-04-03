b9ac4e13c219c1a92751891f3c7189f7b44ff7f8
marinasvirel <marusenka_83@mail.ru>
2025-04-04 01:08:52 +0300

    unless

--- resources/views/users-show/user.blade.php
+++ resources/views/users-show/user.blade.php
@@ -2,30 +2,9 @@
   <x-slot:title>
     {{ $title }}
     </x-slot>
-    @if ($age > 18)
-    <p class="{{ $class }}">Фамилия: {{ $surname }}</p>
-    <p class="{{ $class }}">Имя: {{ $name }}</p>
-    <p class="{{ $class }}">Возраст: {{ $age }}</p>
-    <p class="{{ $class }}">Зарплата: {{ $salary }}</p>
-    <a href="{{ $href }}">{{$text}}</a>
-    <input type="text" value="{{ $value1 }}">
-    <input type="text" value="{{ $value2 }}">
-    <input type="text" value="{{ $value3 }}">
-    <p>current timestamp: {{ date('d.m.Y') }}</p>
-    <p>{{ $arr['name'] }}</p>
-    <p>{{ $arr['age'] }}</p>
-    <p>{{ $arr['salary'] }}</p>
-    <div>{{ count($arr) }}</div>
-    <p>{{-- $city ?? 'Москва' --}}</p>
-    <p>{ {$location['country'] ?? 'Россия' }}</p>
-    <p>{{ $location['city'] ?? 'Москва' }}</p>
-    <p>{{ $year ?? date('Y') }}</p>
-    <p>{{ $month ?? date('m') }}</p>
-    <p>{{ $day ?? date('d') }}</p>
-    <p>{!! $str !!}</p>
-    @elseif ($age === 18)
-    вам 18
-    @else
-    вам меньше 18
-    @endif
+    @unless ($age < 18)
+    несовершеннолетний
+    @endunless
 </x-layout>
+
+{{-- не работает --}}
