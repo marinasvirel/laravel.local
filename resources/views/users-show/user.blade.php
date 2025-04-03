43c20254db214617754602d393520a88f63457d6
marinasvirel <marusenka_83@mail.ru>
2025-04-04 01:44:48 +0300

    Условия и циклы

--- resources/views/users-show/user.blade.php
+++ resources/views/users-show/user.blade.php
@@ -2,7 +2,22 @@
   <x-slot:title>
     {{ $title }}
     </x-slot>
-    @foreach ($strs as $key => $elem)
-    {{ $key + 1 }} {{ $elem }}
-    @endforeach
+    <ul>
+      @foreach ($num as $elem)
+      @if ($elem % 2 == 0)
+      <li>{{ $elem }}</li>
+      @endif
+      @endforeach
+    </ul>
+
+    @if(is_array($data))
+    <ul>
+      @foreach ($data as $elem)
+      <li>{{ $elem }}</li>
+      @endforeach
+    </ul>
+    @elseif(is_numeric($data))
+    {{ $data }}
+    @endif
+
 </x-layout>
