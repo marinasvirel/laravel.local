87330baf52ad8f29467b636e75f2f71c85d43920
marinasvirel <marusenka_83@mail.ru>
2025-04-05 01:51:03 +0300

    for

--- resources/views/users-show/user.blade.php
+++ resources/views/users-show/user.blade.php
@@ -2,34 +2,8 @@
   <x-slot:title>
     {{ $title }}
     </x-slot>
-    <ul>
-      @foreach($strs as $elem)
-      @if ($loop->first)
-      <li class="first">
-        {{ $loop->iteration }}
-        {{ $elem }}
-      </li>
-      @elseif ($loop->last)
-      <li class="last">
-        {{ $loop->iteration }}
-        {{ $elem }}
-      </li>
-      @else
-      <li>
-        {{ $loop->iteration }}
-        {{ $elem }}
-      </li>
-      @endif
-      @endforeach
-    </ul>
-
-    @foreach ($num as $elem)
-    @if($loop->remaining == 2 || $loop->remaining == 1 || $loop->remaining == 0)
-    <i>{{ $elem }}</i>
-    @else
-    <b>{{ $elem }}</b>
-    @endif
-    @endforeach
-
+    @for ($i = 0; $i <= 10; $i++)
+      <p>{{ $i }}</p>
+      @endfor
 
 </x-layout>
