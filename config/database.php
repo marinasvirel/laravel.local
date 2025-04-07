a7a3760d6c953b31d15d69bfe979d3ae9978fae7
marinasvirel <marusenka_83@mail.ru>
2025-04-07 23:02:04 +0300

    Подготовка к изменению полей в миграциях

--- config/database.php
+++ config/database.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Support\Str;
 
+use Illuminate\Database\DBAL\TimestampType;
+
+
+
 return [
+    'dbal' => [
+        'types' => [
+            'timestamp' => TimestampType::class,
+        ],
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -123,7 +132,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
