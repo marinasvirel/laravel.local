8387d18fd88586952fb3407b8db8a93b65cd970b
marinasvirel <marusenka_83@mail.ru>
2025-04-07 22:21:05 +0300

    Запуск миграций

--- database/migrations/2025_04_07_191227_create_users_table.php
+++ database/migrations/2025_04_07_191227_create_users_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('surname');
         });
     }
 
